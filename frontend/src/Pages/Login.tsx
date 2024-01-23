db3e083be6788b66aaef95878beeed75be5b89fe
CrimsonCoder42 <nostro37@gmail.com>
2024-01-23 11:52:14 -0500

    login added box is slightly off

--- frontend/src/Pages/Login.tsx
+++ frontend/src/Pages/Login.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, Typography, Button } from '@mui/material'
+import CustomizedInput from '../components/shared/CustomizedInput'
+
 
 const Login = () => {
   return <Box width={'100%'} height={"100%"} display="flex" flex={1}>  
@@ -10,7 +12,56 @@ const Login = () => {
       >
         <img src="airobot.png" alt="Robot" style={{width: "400px"}} />
       </Box>
-    <Box display={'flex'} flex={{xs: 1, md: 0.5}}>
+    <Box 
+      display={"flex"}
+      flex={{ xs: 1, md: 0.5 }}
+      justifyContent={"center"}
+      alignItems={"center"}
+      padding={2}
+      ml={"auto"}
+      mt={16}
+      >
+        <form 
+        action="" 
+        style={{
+          margin: "auto",
+          padding: "30px",
+          boxShadow: "10px 10px 20px #000",
+          borderRadius: "10px",
+          border: "none",
+        }}
+          >
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }} >
+              <Typography 
+                variant="h4" 
+                textAlign="center"
+                padding={2}
+                fontWeight={600}
+
+                >
+                Login
+              </Typography>
+              <CustomizedInput name="email" type="email" label="Email" />
+              <CustomizedInput name="password" type="password" label="Password" />
+              <Button 
+                type="submit"
+                sx={{px:2, py:1, mt:2, borderRadius: 10, 
+                  bgcolor: '#3f51b5', 
+                  color: 'white',
+                  ":hover":{
+                  bgcolor: 'white',
+                  color: 'black'
+                }}}
+                >
+                Login
+              </Button>
+
+            </Box>
+          </form>
 
     </Box>
   
